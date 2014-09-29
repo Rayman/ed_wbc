e5dc88dc09fff499051984f53c56a9dd57e359e9
Ramon Wijnands <rayman747@hotmail.com>
2014-09-29 16:30:15 +0200

    add some more comments

--- src/edworld.h
+++ src/edworld.h
@@ -11,6 +11,14 @@ class EdWorld : public World
   public:
     EdWorld();
 
+    /**
+     * @brief Initialize world with a set of collision objects
+     *
+     * A collision manager contains pointer's to CollisionObjects.
+     * This means that a manager is not self contained. This class however,
+     * is self contained. It creates a copy of the objects vector. Each element
+     * is a boost::shared_ptr to an object.
+     */
     EdWorld(std::vector< boost::shared_ptr<fcl::CollisionObject> > objects);
 
     fcl::BroadPhaseCollisionManager* getCollisionManager();
